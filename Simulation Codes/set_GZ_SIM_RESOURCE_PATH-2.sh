d56ce79c9e832aa43dcad65b3cd4526293441226
No-Name <68436564+theunknowninfinite@users.noreply.github.com>
2024-07-06 22:47:20 -0400

    Updated readme to setup docker and px4 sim

--- Simulation Codes/set_GZ_SIM_RESOURCE_PATH-2.sh
+++ Simulation Codes/set_GZ_SIM_RESOURCE_PATH-2.sh
@@ -1,3 +1,3 @@
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:/home/root/r2_sim_humble/models
+export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:"/home/root/Simulation Codes/models"
 export GZ_VERSION=garden
 
